af77ad5eb6908b8fd2b71986077fb737ee21cf0d
Sasha <sasha7692@gmail.com>
2018-01-26 00:05:07 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,15 +6,9 @@ public class Main {
     public static void main(String[] args) {
         PublicKeyGeneration pkg = new PublicKeyGeneration();
 
-        int s = 128;
-        int w = 8;
-        String Message = "Hello";
         Scanner nCount = new Scanner(System.in);
         System.out.printf("Input parametr N (power of 2):\n");
         int N = Integer.parseInt(nCount.nextLine());
-        for(int i = 0; i < N; i++) {
-            System.out.println(pkg.merkleKeyGeneration(s, w));
-        }
-
+        pkg.treeBilding(pkg.creationKeysArray(N), N);
     }
 }
