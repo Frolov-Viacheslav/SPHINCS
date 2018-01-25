c2104de121f4c76c892c6dd6b17280a704b99286
Frolov-Viacheslav <frolov_vyacheslav21@mail.ru>
2018-01-26 00:11:18 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,9 +6,11 @@ public class Main {
     public static void main(String[] args) {
         PublicKeyGeneration pkg = new PublicKeyGeneration();
 
+        int s = 128;
+        int w = 8;
         Scanner nCount = new Scanner(System.in);
         System.out.printf("Input parametr N (power of 2):\n");
         int N = Integer.parseInt(nCount.nextLine());
-        pkg.treeBilding(pkg.creationKeysArray(N), N);
+        pkg.treeBilding(pkg.creationKeysArray(s, w, N), N);
     }
 }
