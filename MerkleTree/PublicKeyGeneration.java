f06361eb5788b7e94a0faadbbb09c949dbc39609
Sasha <sasha7692@gmail.com>
2018-02-15 17:24:24 +0200

    Update PublicKeyGeneration.java

--- MerkleTree/PublicKeyGeneration.java
+++ MerkleTree/PublicKeyGeneration.java
@@ -26,9 +26,10 @@ public class PublicKeyGeneration {
         return keysArray;
     }
 
+    public static String root;
+
     public String treeBilding(String [][] keysArray, Integer N){
         int countLayer = (int)Math.ceil(Binarylog.binlog((double) N)) + 1;
-        String root = "";
         String[][] tree = new String[countLayer][N];
         for(int i = 0; i < N; i++) {
             tree[0][i] = keysArray[1][i];
@@ -42,13 +43,13 @@ public class PublicKeyGeneration {
             k = 0;
         }
 
-
         for (int i = 0; i < countLayer; i++) {
             for (int j = 0; j < N; j++) {
                 System.out.print(tree[i][j] + "\t");
             }
             System.out.println();
         }
+        root = tree[countLayer-1][0];
         return root;
     }
 }
