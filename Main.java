2d3cd78006d309c69c74025b4f086ce5e3745085
Frolov-Viacheslav <frolov_vyacheslav21@mail.ru>
2018-02-16 15:41:31 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 import MerkleTree.PublicKeyGeneration;
 import MerkleTree.SignatureGeneration;
 import Config.*;
+import MerkleTree.SignatureVerification;
+
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         PublicKeyGeneration pkg = new PublicKeyGeneration();
         SignatureGeneration sg = new SignatureGeneration();
+        SignatureVerification sv = new SignatureVerification();
         MD5Binary md5b = new MD5Binary();
         int s = 128;
         int w = 4;
@@ -19,7 +22,10 @@ public class Main {
         pkg.treeBilding(pkg.creationKeysArray(s, w, N), N);
 
         String key = pkg.keysArray[1][0];
+        String Signature = sg.SignatureGeneration(key, Message, s, w, pkg.tree, N, pkg.countLayer, pkg.root);
 
-        System.out.println(sg.SignatureGeneration(key, Message, s, w, pkg.tree, N, pkg.countLayer, pkg.root));
+        System.out.println("Signature = " + Signature);
+        String authpath = sv.getAuthPath(Signature, s, pkg.countLayer);
+        sv.merkleKeyVerify(key, pkg.countLayer, authpath, s);
     }
 }
