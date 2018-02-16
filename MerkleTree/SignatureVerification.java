3f5a97c2ccb477b9a7c9cc447185b3de963f754d
Frolov-Viacheslav <frolov_vyacheslav21@mail.ru>
2018-02-16 15:40:13 +0200

    Update SignatureVerification.java

--- MerkleTree/SignatureVerification.java
+++ MerkleTree/SignatureVerification.java
@@ -1,8 +1,9 @@
 package MerkleTree;
-
+import Config.*;
 public class SignatureVerification {
 
 WOTS_CR.SignatureVerification sv = new WOTS_CR.SignatureVerification();
+    MD5HEX md5H = new MD5HEX();
 
     public boolean oneTimeSignatureVerify(String SIGNATURE, String Message, Integer s, Integer w){
         boolean equalSignature = false;
@@ -10,11 +11,19 @@ WOTS_CR.SignatureVerification sv = new WOTS_CR.SignatureVerification();
         return  equalSignature;
     }
 
-    public boolean merkleKeyVerify(){
-        boolean equalSignature = false;
-        return  equalSignature;
+    public void merkleKeyVerify(String key, Integer countLayer, String authPath, Integer s){
+        //boolean equalSignature = false;
+        String root = key;
+        for(int i = 0; i < countLayer - 1; i++) {
+            String temp = authPath;
+            temp = temp.substring(i * s/4, i * s/4 + s/4); // нахождение подстроки с длиной в s символ
+            root += temp;
+            root = md5H.md5Custom(root);
+        }
+        System.out.println("root = " + root);
+        //return  equalSignature;
     }
-    
+
     public String getAuthPath(String SIGNATURE, Integer s, Integer countLayer){
         String authPath;
         authPath = SIGNATURE.substring(s/4 * 2, (s/4 * 2) + (s/4 * (countLayer - 1)));
