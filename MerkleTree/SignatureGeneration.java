4608866d8d0885947d7b6c7788041bf03734079b
Frolov-Viacheslav <frolov_vyacheslav21@mail.ru>
2018-02-23 15:48:56 +0200

    Update SignatureGeneration.java

--- MerkleTree/SignatureGeneration.java
+++ MerkleTree/SignatureGeneration.java
@@ -1,14 +1,13 @@
 package MerkleTree;
-import Config.*;
-import WOTS_CR.*;
 
 public class SignatureGeneration {
-
     WOTS_CR.SignatureGeneration sg = new WOTS_CR.SignatureGeneration();
-   // public static String SIGNATURE = "";
+    public static String OneTimeSignature = "";
+    public  String authPathBit = "";
 
     public String authPathCalculate(String key, String [][] tree, Integer N, Integer countLayer){
         int keyIndex = 0;
+        authPathBit = "";
         String authPath = "";
         for(int i = 0; i < N; i++) {
             if(tree[0][i] == key)
@@ -17,9 +16,11 @@ public class SignatureGeneration {
         for(int i = 0; i < countLayer - 1; i++) {
             if(keyIndex % 2 == 0){
                 keyIndex ++;
+                authPathBit += 0;
             }
             else {
                 keyIndex --;
+                authPathBit += 1;
             }
             authPath += tree[i][keyIndex];
             keyIndex /= 2;
@@ -27,14 +28,15 @@ public class SignatureGeneration {
         return authPath;
     }
 
-    public String oneTimeSignatureGeneration(String Message, Integer s, Integer w) {
-        sg.generateSignature(Message, s, w);
+
+    public String oneTimeSignatureGeneration(String Message, Integer s, Integer w, String X) {
+        sg.generateSignature(Message, s, w, X);
+        OneTimeSignature = sg.SIGNATURE;
         return sg.SIGNATURE;
     }
 
-    public String SignatureGeneration(String key, String Message, Integer s, Integer w, String [][] tree, Integer N, Integer countLayer, String root){
-        MD5HEX md5H = new MD5HEX();
-        String SIGNATURE = key + md5H.md5Custom(oneTimeSignatureGeneration(Message, s, w)) + authPathCalculate(key, tree, N, countLayer) + root;
+    public String SignatureGeneration(String key, String Message, Integer s, Integer w, String [][] tree, Integer N, Integer countLayer, String root, String X){
+        String SIGNATURE = key + oneTimeSignatureGeneration(Message, s, w, X) + authPathCalculate(key, tree, N, countLayer) + authPathBit + root;
         return SIGNATURE;// oneTimeY + oneTimeSig + authPath + root
     }
 }
